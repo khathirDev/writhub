8fda8ac70db9968c28d9bd761553f68c64f804e6
khathir <moyokhathir@gmail.com>
2025-07-03 15:28:48 +0100

    Made updates to the logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,8 +10,9 @@ const Header = ({ children, className }: HeaderProps) => {
   return (
     <div className={cn("header", className)}>
         <Link href={'/'} className='md:flex-1'>
-        <Image src="/assets/icons/logo.svg" alt="logo with name"  width={120} height={32} className='hidden md:block' />
+        {/* <Image src="/assets/icons/logo.svg" alt="logo with name"  width={120} height={32} className='hidden md:block' /> */}
         <Image src="/assets/icons/logo-icon.svg" alt="logo"  width={32} height={32} className='md:hidden mr-2' />
+        <span className='text-2xl font-semibold hidden md:inline'>Writhub</span>
         </Link>
         {children}
     </div>
